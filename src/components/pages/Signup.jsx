c0a3435ae11c479469fb1d80c3367d76bd648888
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-04-20 12:21:29 +0900

    feat: Signupコンポーネント新規作成 #12

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,25 +1,55 @@
-import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, Input, Stack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const outerBoxStyles = {
+    background: 'url(https://source.unsplash.com/SAS0lq2QGLs) center/cover no-repeat',
+    backgroundPosition: 'bottom top',
+  }
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
   return (
-    <Flex align="center" justify="center" height="100vh">
-      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
-        <Heading as="h1" size="lg" textAlign="center" >アカウント登録ページです</Heading>
-        <Divider my={4} />
-        <form>
-          <Stack spacing={4} py={4} px={10}>
-            <Input placeholder="メールアドレス" value="" />
-            <Input placeholder="パスワード" value="" />
-            <Button
-              colorScheme='teal'
-              isDisabled=""
-            >
-              登録
-            </Button>
-          </Stack>
-        </form>
-      </Box>
-    </Flex>
+    <>
+      <Flex align="center" justify="center" height="90vh">
+        <Box h="lg" w="lg" p={20} borderRadius="xl" shadow="md" sx={outerBoxStyles} color="purple.800">
+          <Box bg="black" border="1px solid white" borderRadius="md" mb={3}>
+            <Heading as="h1" size="lg" textAlign="center" color="white" p={3}>アカウント登録ページ</Heading>
+          </Box>
+          <form>
+            <Stack spacing={6} pt={10} px={8} >
+              <Input type="email" placeholder="メールアドレス" value={email} onChange={onChangeEmail} bg="white"/>
+              <Input type="password" placeholder="パスワード" value={password} onChange={onChangePassword} bg="white"/>
+              <Button
+                colorScheme='teal'
+                isDisabled=""
+              >
+                登録する(無料)
+              </Button>
+            </Stack>
+          </form>
+          <Box textAlign="center" shadow="md">
+            <Divider my={4} />
+
+            <Text color="white" mt={4} mb={6}>
+              アカウントをお持ちの方
+            </Text>
+            <Link to='/login' style={{ backgroundColor: '#2874aa', borderRadius: "10%", padding: '8px', color: "white" }}>ログイン</Link>
+          </Box>
+        </Box>
+      </Flex>
+    </>
   )
 }
 
