f1956dbb00b2c251bed54c75854ed9a9f60da058
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-04-23 13:22:47 +0900

    feat: drawerとハンバーガーアイコンボタンを新規追加 #27

---
+++ src/components/molecules/CustomDrawer.jsx
@@ -0,0 +1,50 @@
+import { Box, Flex, Button, VStack, StackDivider } from "@chakra-ui/react";
+import { memo } from "react";
+
+export const CustomDrawer = memo(({ isOpen, handleOverlayClick, onClickHome, onClickLogin, onClickSignup }) => {
+
+  return (
+    <>
+      {isOpen && (
+        <Box
+          bg="rgba(0, 0, 0, 0.5)"
+          position="fixed"
+          top="0"
+          left="0"
+          height="100vh"
+          width="100vw"
+          zIndex="10"
+          onClick={handleOverlayClick}
+        >
+          <Box
+            
+            bg="gray.100"
+            position="absolute"
+            top="0"
+            right="0"
+            height="100vh"
+            width="250px"
+            boxShadow="-2px 0px 5px rgba(0,0,0,0.25)"
+            zIndex="20"
+          >
+            <Flex justify="flex-end" padding="10px">
+              <Button 
+              color="purple.800" border="2px solid gray" bg="url(https://source.unsplash.com/mIT9kEQBC6Q)" onClick={handleOverlayClick}>
+                Close
+              </Button>
+            </Flex>
+            <VStack
+              divider={<StackDivider borderColor='gray.200' />}
+              spacing={1}
+              align='stretch'
+            >
+              <Button border="2px solid gray" color="purple.800" bg="gray.300" w="100%" onClick={onClickHome}>Home</Button>
+              <Button border="2px solid gray" color="purple.800" bg="gray.300" w="100%" onClick={onClickLogin}>ログイン</Button>
+              <Button border="2px solid gray" color="purple.800" bg="gray.300" w="100%" onClick={onClickSignup}>アカウント登録</Button>
+            </VStack>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+});
