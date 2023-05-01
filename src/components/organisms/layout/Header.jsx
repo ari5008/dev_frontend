bf410fbde5d9f54c99f55489e8259cda7a971e48
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-01 21:08:08 +0900

    refactor: headerのdrawerのリファクタリング #27

--- src/components/organisms/layout/Header.jsx
+++ src/components/organisms/layout/Header.jsx
@@ -1,11 +1,11 @@
 import { memo } from "react";
 import { Flex, Box, useDisclosure } from "@chakra-ui/react"
 import { Link, useNavigate } from "react-router-dom";
-import { CustomDrawer } from "../../molecules/CustomDrawer";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import "../../../styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { SmallScreenDrawer } from "../../molecules/drawer/SmallScreenDrawer";
 
 export const Header = memo(() => {
 
@@ -34,11 +34,11 @@ export const Header = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 3 }}
         position="fixed"
-        zIndex="1"
+        zIndex="10"
         top="0"
         width="100%"
       >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex align="center" mr={8} _hover={{ cursor: "pointer" }}>
           <Box fontSize="lg" textShadow="2px 0px 2px #0b0c0d">
             <Link to="/" className="link">
               <span>曲選出</span>
@@ -66,13 +66,13 @@ export const Header = memo(() => {
               <span>
                 新規登録
                 <FontAwesomeIcon icon={faUserPlus} />
-                </span>
+              </span>
             </Link>
           </Box>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <CustomDrawer isOpen={isOpen} handleOverlayClick={handleOverlayClick} onClickHome={onClickHome} onClickLogin={onClickLogin} onClickSignup={onClickSignup} onClickViewTopic={onClickViewTopic} />
+      <SmallScreenDrawer isOpen={isOpen} handleOverlayClick={handleOverlayClick} onClickHome={onClickHome} onClickLogin={onClickLogin} onClickSignup={onClickSignup} onClickViewTopic={onClickViewTopic} />
     </>
   )
 })
