f9ce28f4cd13982bc8d31368a339981d395be6da
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-12 17:17:00 +0900

    feat: ログインユーザーのproviderを新規作成 #74

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { LoginUserProvider } from './providers/LoginUserProvider'
 
 const queryClient = new QueryClient({})
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <LoginUserProvider>
         <App />
+      </LoginUserProvider>
     </QueryClientProvider>
   </React.StrictMode>
 )
