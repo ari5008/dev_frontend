e169020261de654b384f6a89d3e4e09535a9c230
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-12 12:35:32 +0900

    feat: vite.configのlocalhostとport変更 #67

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import dns from 'dns'
 
+dns.setDefaultResultOrder('verbatim')
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
-export default {
-  plugins: [react()]
-};
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000,
+    strictPort: true,
+  },
+});
