5c0ef6f5325cd1f318064287a4829a83e371d6d0
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-13 12:14:30 +0900

    feat: エラーページ新規作成 #88

---
+++ src/components/pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import { Box, Heading, Text } from "@chakra-ui/react"
+import { memo } from "react"
+
+export const ErrorPage = memo(() => {
+  return (
+    <>
+      <Box display="flex" justifyContent="center" height="100vh">
+        <Box textAlign="center" pt="3rem">
+          <Heading fontSize="6xl">404</Heading>
+          <Text fontSize="2xl">page not found</Text>
+          <Text fontSize="2xl" pt="3rem" textAlign="left">申し訳ございません。お探しのページは見つかりませんでした。</Text>
+          <Text fontSize="2xl" textAlign="left">ページが存在しないか、予期しないエラーが発生しました。</Text>
+          <br/>
+        </Box>
+      </Box>
+    </>
+  )
+})
