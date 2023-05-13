48a79276d479fc509f9ead5ff39230aa2ffac61a
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-13 11:36:01 +0900

    style: MenuIconButtonからSmallScreenMenuButtonにファイル名変更

--- src/components/atoms/button/SmallScreenMenuButton.jsx
+++ src/components/atoms/button/SmallScreenMenuButton.jsx
@@ -2,13 +2,12 @@ import { IconButton } from "@chakra-ui/button"
 import { HamburgerIcon } from "@chakra-ui/icons"
 import { memo } from "react"
 
-export const MenuIconButton = memo(({onOpen}) => {
+export const SmallScreenMenuButton = memo(({onOpen}) => {
     
     return (
         <IconButton
         aria-label="メニューボタン" 
-        icon={<HamburgerIcon />} 
-        size="xl" 
+        icon={<HamburgerIcon fontSize='20px'/>} 
         variant="unstyled" 
         display={{ base: "block", lg: "none" }} 
         onClick={onOpen} 
