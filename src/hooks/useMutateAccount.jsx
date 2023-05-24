6fcccd7e3b1ffb312f4a75608713b380204e0fe6
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-24 22:38:43 +0900

    feat: useMutateAccount新規作成 #5

---
+++ src/hooks/useMutateAccount.jsx
@@ -0,0 +1,61 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
+import { useError } from "./useError";
+
+export const useMutateAccount = () => {
+
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+  const { showMessage } = useMessage()
+  const { switchErrorHandling } = useError()
+
+  const getAccountMutation = useMutation(
+    () =>
+      axios.get(`${import.meta.env.VITE_API_URL}/account`, { withCredentials: true }),
+    {
+      onSuccess: (res) => {
+        return res.data
+      },
+      onError: (err) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+
+  const updateAccountMutation = useMutation(
+    (account) =>
+      axios.put(`${import.meta.env.VITE_API_URL}/account/${account.id}`, {
+        name: account.name,
+        image_url: account.image_url,
+        introduction: account.introduction,
+      }),
+    {
+      onSuccess: (res, variables) => {
+        const previousAccount = queryClient.getQueryData(['account'])
+        if (previousAccount) {
+          queryClient.setQueryData(
+            ['account'],
+            previousAccount.id === variables.id ? res.data : previousAccount
+          )
+        }
+        showMessage({ title: "登録しました", status: "success" })
+        navigate(`/account/${res.data.id}`)
+      },
+      onError: (err) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+
+  return { getAccountMutation, updateAccountMutation }
+}
