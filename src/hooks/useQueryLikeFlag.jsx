39a3828d451e809aa593407b705f6c6ad4fafe23
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-22 14:14:08 +0900

    feat: useQueryLikeFlag新規作成 #5

---
+++ src/hooks/useQueryLikeFlag.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useError } from './useError';
+import { useQuery } from '@tanstack/react-query';
+
+export const useQueryLikeFlag = (trackId, accountId) => {
+  const { switchErrorHandling } = useError();
+  const getLikeFlag = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/account/getLikeFlag/${trackId}`,
+      { withCredentials: true }
+    );
+    return data;
+  };
+  return useQuery({
+    queryKey: ["likeFlag", trackId, accountId],
+    queryFn: getLikeFlag,
+    staleTime: Infinity,
+    onError: (err) => {
+      if (err.response.data.message) {
+        switchErrorHandling(err.response.data.message);
+      } else {
+        switchErrorHandling(err.response.data);
+      }
+    },
+  });
+};
