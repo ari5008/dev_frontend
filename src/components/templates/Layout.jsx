db09ca6137f38dc4aab27487733aa4dba6236889
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-23 18:32:17 +0900

    style: Layoutのnavを削除する #39

--- src/components/templates/Layout.jsx
+++ src/components/templates/Layout.jsx
@@ -1,21 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { Header } from "../organisms/layout/Header";
-import { Navbar } from '../organisms/layout/Navbar';
-import { useIsSmallScreen } from "../../hooks/useIsSmallScreen";
 
 export const Layout = ({ children }) => {
-
-  const isSmallScreen = useIsSmallScreen();
 
   return (
     <>
       <Header />
       <Box position="relative">
-        <Box w={{ base: "0%", lg: "9%" }} bg="white" h="100vh" position="fixed" zIndex="5">
-          {isSmallScreen ? null : <Navbar />}
-        </Box>
         <Box
-          ml={{ base: "0%", lg: "9%" }}
           pt="4rem"
         >
           <Box
