6a1eb7410fa9d05c3ae78edfd5ee7ee8a0f91fff
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-07-04 18:03:53 +0900

    feat: AccountAvatarの保存方法変更 #32

--- src/components/atoms/avatar/AccountAvatar.jsx
+++ src/components/atoms/avatar/AccountAvatar.jsx
@@ -6,9 +6,36 @@ export const AccountAvatar = memo(({ updateAccount, editedAccount }) => {
   const handleFileSelect = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
-    reader.onload = (upload) => {
-      updateAccount({ ...editedAccount, image_url: upload.target.result });
+
+    // 画像のリサイズ処理
+    const handleImageResize = (img) => {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+
+      // リサイズ後の幅と高さを指定（例: 幅が300pxでアスペクト比を保持）
+      const maxWidth = 50;
+      const maxHeight = img.height * (maxWidth / img.width);
+
+      // canvasに描画してリサイズ
+      canvas.width = maxWidth;
+      canvas.height = maxHeight;
+      context.drawImage(img, 0, 0, maxWidth, maxHeight);
+
+      // リサイズ後の画像をBase64形式で取得
+      const resizedImageUrl = canvas.toDataURL(file.type);
+
+      // アップデート処理
+      updateAccount({ ...editedAccount, image_url: resizedImageUrl });
     };
+
+    reader.onload = (upload) => {
+      const img = new Image();
+      img.onload = () => {
+        handleImageResize(img);
+      };
+      img.src = upload.target.result;
+    };
+
     reader.readAsDataURL(file);
   };
 
